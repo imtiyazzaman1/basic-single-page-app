5fc5fb28fe6c4070e282a8ef7ef47cdc44c29871
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-13 18:13:56 +0000

    index page set up

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var PORT = "8080";
+var HOST = "127.0.0.1";
+
+app.set("view engine", 'ejs');
+
+app.get('/', function (req, res) {
+  res.render("index");
+});
+
+app.listen(PORT, () => {
+  console.log("server up")
+});
