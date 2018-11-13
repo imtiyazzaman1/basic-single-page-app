996d8ca594ef1d56fdf2b7e104b3389c97e1bd2a
imtiyazzaman1 <imtiyaz.zaman@live.co.uk>
2018-11-13 18:55:49 +0000

    added public folder

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+var path = require('path')
 var PORT = "8080";
 var HOST = "127.0.0.1";
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set("view engine", 'ejs');
 
